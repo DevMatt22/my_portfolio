8a9229b06aa4d856959be7552be04ac7dc83eab4
Matthieu MARTIN <martin.matt820@gmail.com>
2022-10-13 18:53:35 +0200

    🎉 Initial commit

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,16 +1,20 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app/app.module';
+import { AppModule } from "./app/app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const config = new DocumentBuilder()
+    .setTitle("Portfolio Swagger")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  const globalPrefix = "api";
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port);
