6f79b9e07184ddceffa189b5fe9975d204527c52
Matthieu MARTIN <martin.matt820@gmail.com>
2022-10-15 01:47:20 +0200

    🔧 Add TypeOrm config

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from "@nestjs/common";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { Project } from "./models/project.model";
+import { ProjectsModule } from "./modules/projects.module";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get("DATABASE_HOST"),
+        port: parseInt(configService.get("DATABASE_PORT")),
+        username: configService.get("DATABASE_USER"),
+        password: configService.get("DATABASE_PASSWORD"),
+        database: configService.get("DATABASE_DB"),
+        entities: [Project],
+        synchronize: true
+      })
+    }),
+    ProjectsModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
