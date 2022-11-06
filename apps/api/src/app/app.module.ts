e8a4ab335675b509b0007f7680cc90ecb314a5a1
Matthieu MARTIN <martin.matt820@gmail.com>
2022-11-06 17:14:13 +0100

    ✨Adding user entity

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
 
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { ProjectsModule } from "./modules/projects.module";
-import { Project } from "./entities/project.entity";
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ProjectsModule } from './modules/projects.module';
+import { Project } from './models/entities/project.entity';
+import { User } from './models/entities/user.entity';
+
 
 @Module({
   imports: [
@@ -13,22 +15,21 @@ import { Project } from "./entities/project.entity";
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: "postgres",
-        host: configService.get("DATABASE_HOST"),
-        port: parseInt(configService.get("DATABASE_PORT")),
-        username: configService.get("DATABASE_USER"),
-        password: configService.get("DATABASE_PASSWORD"),
-        database: configService.get("DATABASE_DB"),
-        entities: [Project],
-        //entities: [join(__dirname, '**', '*.entity.{ts,js}')],
-        logging: "all",
-        synchronize: true
-      })
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: parseInt(configService.get('DATABASE_PORT')),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_DB'),
+        entities: [Project, User],
+        logging: 'all',
+        synchronize: true,
+      }),
     }),
-    ProjectsModule
+    ProjectsModule,
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
 export class AppModule {
 }
