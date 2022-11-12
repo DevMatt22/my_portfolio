e1e6abf6cf8b43bb7baab33976ab8f7a8c7a3e97
Matthieu MARTIN <martin.matt820@gmail.com>
2022-11-12 22:35:33 +0100

    ⬆ Update project

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,23 +2,23 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = ('api');
-  const config = new DocumentBuilder()
-    .setTitle('Portfolio Swagger')
-    .setVersion('1.0')
-    .build();
+	const app = await NestFactory.create(AppModule);
+	app.enableCors();
+	app.use(csurf());
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(`${globalPrefix}/swagger`, app, document);
+	const globalPrefix = 'api';
 
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix} 🚀`,
-  );
+	const config = new DocumentBuilder().setTitle('Portfolio Swagger').setVersion('1.0').build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup(`${globalPrefix}/swagger`, app, document);
+
+	const port = process.env.PORT || 3333;
+	await app.listen(port);
+	Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix} 🚀`);
 }
 
 bootstrap();
