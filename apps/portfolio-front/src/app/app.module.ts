660e7b4668b3d3137a09afc5e69c84e16a7e6155
DevMatt <martin.matt820@gmail.com>
2023-01-08 12:14:58 +0100

    :art: Set project language to french

--- apps/portfolio-front/src/app/app.module.ts
+++ apps/portfolio-front/src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeFr);
 
 @NgModule({
-	declarations: [AppComponent, NxWelcomeComponent, HeaderComponent],
+	declarations: [AppComponent, NxWelcomeComponent],
 	imports: [
 		BrowserModule,
 		HttpClientModule,
@@ -22,7 +26,12 @@ import { MatTabsModule } from '@angular/material/tabs';
 		MatButtonModule,
 		MatTabsModule,
 	],
-	providers: [],
+	providers: [
+		{
+			provide: LOCALE_ID,
+			useValue: 'fr-FR',
+		},
+	],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
