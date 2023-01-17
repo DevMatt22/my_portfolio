5306fc945da93cc81126ca5f6c296fc539bf3153
DevMatt <martin.matt820@gmail.com>
2023-01-17 23:08:33 +0100

    :fire: remove user entity

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -6,28 +6,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ProjectsModule } from './modules/projects.module';
 import { Project } from './models/entities/project.entity';
-import { User } from './models/entities/user.entity';
-import { UsersModule } from './modules/user.module';
 
 @Module({
 	imports: [
 		TypeOrmModule.forRootAsync({
 			imports: [ConfigModule],
 			inject: [ConfigService],
-			useFactory: (configService: ConfigService) => ({
-				type: 'postgres',
-				host: configService.get('DATABASE_HOST'),
-				port: parseInt(configService.get('DATABASE_PORT')),
-				username: configService.get('DATABASE_USER'),
-				password: configService.get('DATABASE_PASSWORD'),
-				database: configService.get('DATABASE_DB'),
-				entities: [Project, User],
-				logging: 'all',
-				synchronize: true,
-			}),
+			useFactory: (configService: ConfigService) => {
+				return {
+					type: 'postgres',
+					host: configService.get('DATABASE_HOST'),
+					port: configService.get('DATABASE_PORT'),
+					username: configService.get('DATABASE_USER'),
+					password: configService.get('DATABASE_PASSWORD'),
+					database: configService.get('DATABASE_DB'),
+					entities: [Project],
+					logging: 'all',
+					synchronize: true,
+				};
+			},
 		}),
 		ProjectsModule,
-		UsersModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
